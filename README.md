073ed3b758c9ceb005dbd5de02491ac074d835e0
Connor Schwirian <cschwirian@gmail.com>
2019-04-15 18:21:07 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# perfectsequencealignment
+# Perfect Sequence Alignment
 A library for the sequence alignment of k-mers based on a perfect read.
---
+++ psa.py
@@ -0,0 +1,12 @@
+class Node:
+
+    def __init__( self ):
+        self.name = "test"
+
+
+def main():
+    print( "main" )
+
+
+if( __name__ == "__main__" ):
+    main()
