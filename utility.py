6ed4644b813ab53dc2b5de63fe26404cea470d9a
cschwirian <cschwirian@gmail.com>
2019-04-15 21:57:30 -0700

    Added read_fasta and k_mer to utility.

--- utility.py
+++ utility.py
@@ -1,11 +1,22 @@
-def read_fasta( filename ):
-    print( filename )
+def read_fasta( filename, text=True ):
+
+    if( text ):
+        with open( filename, "rb" ) as sequence:
+            header = sequence.readline().replace( b"\n", b"" ).replace( b">", b"" )
+            sequence_string = sequence.read().replace( b"\n", b"" )
+            return { header: sequence_string }
+    else:
+        with open( filename, "r" ) as sequence:
+            header = sequence.readline().replace( "\n", "" ).replace( ">", "" )
+            sequence_string = sequence.read().replace( "\n", "" )
+            return { header: sequence_string }
 
 
 def k_mers( sequence, k ):
-    print( "k" * k )
 
+    mers = []
 
-if( __name__ == "__main__" ):
-    read_fasta( test )
-    k_mers( "test", 4 )
+    for index in range( len( sequence ) - k + 1 ):
+        mers.append( sequence[index:index + k] )
+
+    return mers
