3c5de171807e5516652e976f1d6d338398359543
Connor Schwirian <cschwirian@gmail.com>
2019-04-15 18:26:21 -0700

    Added file for utility functions.

---
+++ utility.py
@@ -0,0 +1,11 @@
+def read_fasta( filename ):
+    print( filename )
+
+
+def k_mers( sequence, k ):
+    print( "k" * k )
+
+
+if( __name__ == "__main__" ):
+    read_fasta( test )
+    k_mers( "test", 4 )
