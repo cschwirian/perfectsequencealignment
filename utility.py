c5e7ffafaa0f70dbe85c8a2f9e641f9a74cdc0b1
Connor Schwirian <cschwirian@gmail.com>
2019-04-22 17:01:55 -0700

    Added utility functions from homework.

--- utility.py
+++ utility.py
@@ -1,9 +1,28 @@
 def read_fasta( filename ):
-    print( filename )
+
+    if( text ):
+        with open( file_name, "rb" ) as sequence:
+            header = sequence.readline().replace( b"\n", b"" ).replace( b">", b"" )
+            sequence_string = sequence.read().replace( b"\n", b"" )
+            return { header: sequence_string }
+    else:
+        with open( file_name, "r" ) as sequence:
+            header = sequence.readline().replace( "\n", "" ).replace( ">", "" )
+            sequence_string = sequence.read().replace( "\n", "" )
+            return { header: sequence_string }
 
 
 def k_mers( sequence, k ):
-    print( "k" * k )
+
+    mer_list = []
+
+    for index in range( len( sequence ) ):
+        substring = sequence[index:index + k]
+        if( len( substring ) != k ):
+            break
+        mer_list.append( substring )
+
+    return mer_list
 
 
 if( __name__ == "__main__" ):
