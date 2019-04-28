9adaf53cfc0f98850c06563476d0ddbe2a3978ff
cschwirian <cschwirian@gmail.com>
2019-04-28 15:17:17 -0700

    Restructured code.

--- src/psa.py
+++ src/psa.py
@@ -1,6 +1,3 @@
-from utility import *
-
-
 # Simple node to contain mer data as well as ordered list of node's children to
 # assist with assembly.
 class Node:
@@ -81,6 +78,34 @@ def psa( filename, k ):
         print( "Sequence assembled successfully.", flush=True )
     else:
         print( "Sequence assembly failed.", flush=True )
+
+
+# Utility Functions
+def read_fasta( filename, text=False ):
+
+    if( text ):
+        with open( filename, "rb" ) as sequence:
+            header = sequence.readline().replace( b"\n", b"" ).replace( b">", b"" )
+            sequence_string = sequence.read().replace( b"\n", b"" )
+            return { header: sequence_string }
+    else:
+        with open( filename, "r" ) as sequence:
+            header = sequence.readline().replace( "\n", "" ).replace( ">", "" )
+            sequence_string = sequence.read().replace( "\n", "" )
+            return { header: sequence_string }
+
+
+def k_mers( sequence, k ):
+
+    mer_list = []
+
+    for index in range( len( sequence ) ):
+        substring = sequence[index:index + k]
+        if( len( substring ) != k ):
+            break
+        mer_list.append( substring )
+
+    return mer_list
 
 
 
